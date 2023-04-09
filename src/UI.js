dcf0373d6a40c8030f5ce334f7e70a1220ed5561
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-04-09 23:51:14 +0530

    Set up the files and basic styling

---
+++ src/UI.js
@@ -0,0 +1,14 @@
+const main = document.querySelector('.main');
+
+const createHeading = () => {
+  const headingDiv = document.createElement('div');
+  const headingPara = document.createElement('p');
+  headingDiv.setAttribute('class', 'heading');
+  headingPara.textContent = 'ToDo List';
+  headingDiv.appendChild(headingPara);
+  main.appendChild(headingDiv);
+};
+
+export default () => {
+  createHeading();
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { default as UI } from './UI';
+
+UI();
