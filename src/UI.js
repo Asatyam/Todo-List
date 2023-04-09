f592a366438b75534ae1af5940ff6f3ed976699d
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-04-10 00:03:55 +0530

    Add the function to display form

--- src/UI.js
+++ src/UI.js
@@ -16,12 +16,38 @@ const createSidebar = () => {
 };
 
 const createContent = () => {
-    const contentDiv = document.createElement('div');
-    contentDiv.setAttribute('class','content');
-    main.appendChild(contentDiv);
-}
+  const contentDiv = document.createElement('div');
+  contentDiv.setAttribute('class', 'content');
+  main.appendChild(contentDiv);
+};
+
+const createProject = () => {
+  const addProjectBtn = document.createElement('button');
+  addProjectBtn.setAttribute('class', 'addProjectBtn');
+  addProjectBtn.textContent = '+ Add Project';
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.appendChild(addProjectBtn);
+};
+
+const createProjectForm = () => {
+  const form = document.createElement('form');
+  form.setAttribute('class', 'project-form');
+  form.innerHTML = `<input
+        type="text"
+        name="project-name"
+        id="project-name"
+        placeholder="Project Name"
+        required
+      />
+      <button type="submit">Add</button>
+      <button type="cancel">Cancel</button>`;
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.appendChild(form);
+};
 export default () => {
   createHeading();
   createSidebar();
   createContent();
+  createProject();
+  createProjectForm();
 };
